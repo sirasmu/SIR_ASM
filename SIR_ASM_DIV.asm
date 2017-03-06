cab379f2235b33c1b5bd87c9937f9bca2331bf04
S. Rasmussen <siras@outlook.com>
2017-03-06 18:25:54 +0100

    Update SIR_ASM_DIV.asm

--- SIR_ASM_DIV.asm
+++ SIR_ASM_DIV.asm
@@ -1,10 +1,10 @@
 ;My own algorithm for dividing in assembly... I'll name it SIR_ASM_DIV
 	.def    a = r16           	;To hold dividend
 	.def    b = r17           	;To hold divisor
-	.def	i = r18			;Count for amounts of time you went though the loop.. This is the result
 	.def	c = r19			;Count for whether you reached the value of a
-	.def	r = r20			;Remainder
 	.def	d = r21			;Used as c-1 to see whether c is greater than a
+	.def	i = r18			;Result. Counts amount of times b can be added to itself before reaching a
+	.def	r = r20			;Remainder
 	ldi a, 6
 	ldi b, 3
 	ldi i, 0

d06aac5501b0778779130a6a561d2f160df5389f
S. Rasmussen <siras@outlook.com>
2017-03-13 15:28:31 +0100

    Update loop.asm

--- loop.asm
+++ loop.asm
@@ -1,5 +1,5 @@
 ;loop of 0x2710 iterations
 ldi		xh, 0x27
 ldi		xl, 0x10
-loop:		st -x, r16
+loop:		st -x, r16; I want to see if there is a better way of doing this, since r16 is of no relevance
 		brne	loop
